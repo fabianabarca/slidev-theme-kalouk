61ddb1ce72193d34a0cc3ad8bf0d9110ff787951
Fabián Abarca <ensinergia@gmail.com>
2025-07-01 12:28:41 -0300

    feat: refined "grid" layout

--- layouts/grid.vue
+++ layouts/grid.vue
@@ -1,16 +1,108 @@
 <template>
-  <div class="slidev-layout grid">
-    <n-grid cols="2 s:3 m:4 l:5 xl:6 2xl:7" responsive="screen">
-      <n-grid-item>
-        <div class="light-green">1</div>
-      </n-grid-item>
-      <n-grid-item>
-        <div class="light-green">2</div>
-      </n-grid-item>
-    </n-grid>
+  <div class="slidev-layout layout w-full h-full">
+    <div class="row-header">
+      <slot />
+    </div>
+    <div class="row-grid">
+      <div class="grid">
+        <div class="card" v-if="$slots.A">
+          <slot name="A" />
+        </div>
+        <div class="card" v-if="$slots.B">
+          <slot name="B" />
+        </div>
+        <div class="card" v-if="$slots.C">
+          <slot name="C" />
+        </div>
+        <div class="card" v-if="$slots.D">
+          <slot name="D" />
+        </div>
+        <div class="card" v-if="$slots.E">
+          <slot name="E" />
+        </div>
+        <div class="card" v-if="$slots.F">
+          <slot name="F" />
+        </div>
+        <div class="card" v-if="$slots.G">
+          <slot name="G" />
+        </div>
+        <div class="card" v-if="$slots.H">
+          <slot name="H" />
+        </div>
+        <div class="card" v-if="$slots.I">
+          <slot name="I" />
+        </div>
+      </div>
+    </div>
+    <div class="row-bottom text-center">
+      <slot name="bottom" />
+    </div>
   </div>
 </template>
 
-<script setup>
-import { NGrid, NGridItem } from "naive-ui";
-</script>
+<style scoped>
+.layout {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  height: 100%;
+  gap: 1rem;
+}
+
+.row-header {
+  grid-row: 1;
+}
+
+.row-grid {
+  grid-row: 2;
+  width: 100%;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.row-bottom {
+  grid-row: 3;
+  align-self: end;
+  font-size: 0.875rem;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  width: 100%;
+  justify-content: center;
+}
+
+@media (min-width: 640px) {
+  .grid {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    max-width: 100%;
+  }
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  border-radius: 0.25rem;
+  padding: 1rem;
+  background-color: var(--ctp-latte-surface0);
+  color: var(--ctp-latte-text);
+  height: 100%;
+  min-width: 200px;
+}
+
+html.dark .card {
+  background: var(--ctp-mocha-surface0);
+  color: var(--ctp-mocha-text);
+}
+
+.card-icon {
+  margin-left: auto;
+}
+</style>
